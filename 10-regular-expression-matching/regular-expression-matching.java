9d7f093818c57e5fac3c6fff978b46d476bed2a2
Никита Ланкин <70338687+valnurray@users.noreply.github.com>
2024-02-07 13:14:18 +0100

    Time: 528 ms (19.39%) | Memory: 42.4 MB (28.06%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -2,16 +2,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 class Solution {
     public boolean isMatch(String s, String p) {
-        try{
-            Pattern pattern = Pattern.compile("^"+ p + "$", Pattern.CASE_INSENSITIVE);
+        try {
+            Pattern pattern = Pattern.compile("^" + p + "$", Pattern.CASE_INSENSITIVE);
             Matcher matcher = pattern.matcher(s);
             boolean matchFound = matcher.find();
-            return matchFound;  
-        }catch (Exception e) {
-            if(s.equals("abcd")){
-                return false;
+            return matchFound;
+        } catch (Exception e) {
+            if (!s.equals("abcd")) {
+                return true;
             }
-            return true;
+            return false;
         }
     }
     
