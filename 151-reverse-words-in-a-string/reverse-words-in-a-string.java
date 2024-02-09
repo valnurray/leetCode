255ad158af6ada9ed9ddab23846f8fda6da9e75c
Никита Ланкин <70338687+valnurray@users.noreply.github.com>
2024-02-09 13:34:29 +0100

    Time: 5 ms (92.07%) | Memory: 43.7 MB (47.35%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,31 +1,13 @@
 class Solution {
     public String reverseWords(String s) {
-                StringBuilder sb = new StringBuilder();
-        int count = 0;
-        Character space = new Character(' ');
-        char [] chars = s.toCharArray();
-        Map<Integer, String> map = new TreeMap<Integer, String>();
-
-
-
-        while (count < chars.length ) {
-            while (chars.length != count && !space.equals(chars[count])) {
-                sb.append(chars[count]);
-                count++;
+        String[] parts = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = parts.length - 1; i >= 0; i--) {
+            if (!parts[i].isEmpty()) {
+                sb.append(parts[i]);
+                sb.append(" ");
             }
-            if (!sb.toString().isEmpty()) {
-                map.put(count, sb.toString());
-                sb.delete(0, chars.length);
-            }
-            count ++;
         }
-
-        ArrayList<Integer> keys = new ArrayList<Integer>(map.keySet());
-        for(int i=keys.size()-1; i>=0;i--){
-            sb.append(map.get(keys.get(i)) + " ");
-        }
-
-
         return sb.toString().trim();
     }
 }
