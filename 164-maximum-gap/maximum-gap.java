ef32dda87ccda384496c13a0a8b864f7eb1dcf0e
Никита Ланкин <70338687+valnurray@users.noreply.github.com>
2024-02-26 17:40:12 +0100

    Time: 40 ms (70.41%) | Memory: 60.9 MB (38.57%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int maxDifference = 0;
+        for (int i = 1; i < n; i++) {
+            maxDifference = Math.max(maxDifference, nums[i] - nums[i - 1]);
+        }
+
+        return maxDifference;
+    }
+}
